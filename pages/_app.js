704569fe63961962a412294c00ae2308502fec52
Davis Zhong <zhong.sw@qq.com>
2021-05-06 11:54:02 +0800

    add GTM

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import { GTMProvider } from '@elgorditosalsero/react-gtm-hook'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const gtmParams = { id: process.env.NEXT_PUBLIC_GTM_ID }
+
+  return (
+    <GTMProvider state={gtmParams}>
+      <Component {...pageProps} />
+    </GTMProvider>
+  )
 }
 
 export default MyApp
